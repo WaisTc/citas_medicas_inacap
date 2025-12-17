80e2d1498c1da631f9aaad012ab76e10540b26d3
WaisTc <javier.torres35@inacapmail.cl>
2025-12-16 22:54:20 -0300

    16-12-2025 22:54

--- middleware/securityMiddleware.js
+++ middleware/securityMiddleware.js
@@ -2,15 +2,18 @@ const helmet = require('helmet');
 
 const securityMiddleware = helmet({
     contentSecurityPolicy: {
+        useDefaults: false,
         directives: {
             defaultSrc: ["'self'"],
             scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://cdn.jsdelivr.net", "https://kit.fontawesome.com", "https://maps.googleapis.com"],
             styleSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://fonts.googleapis.com"],
             fontSrc: ["'self'", "https://cdn.jsdelivr.net", "https://ka-f.fontawesome.com", "https://fonts.gstatic.com"],
-            imgSrc: ["'self'", "data:", "https://maps.gstatic.com", "https://maps.googleapis.com"],
-            connectSrc: ["'self'", "https://ka-f.fontawesome.com", "https://maps.googleapis.com"],
+            imgSrc: ["'self'", "data:", "https://maps.gstatic.com", "https://maps.googleapis.com", "https://img.freepik.com", "https://placehold.co"], // Added common image sources just in case
+            connectSrc: ["'self'", "https://ka-f.fontawesome.com", "https://maps.googleapis.com", "https://cdn.jsdelivr.net"],
+            upgradeInsecureRequests: null, // Explicitly disable auto-upgrade to HTTPS
         },
     },
+    strictTransportSecurity: false, // Disable HSTS to allow HTTP
 });
 
 module.exports = securityMiddleware;
